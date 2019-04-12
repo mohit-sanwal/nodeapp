e6eebb04c3243ca5c405f65451eb8eefebeffc22
mohit sanwal <mohits@dewsolutions.in>
2019-04-12 13:14:51 +0530

    start script changed

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
     res.send('res....');
 })
@@ -27,4 +29,6 @@ app.get('/about', function(req, res) {
     });
 })
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log('port is up on:${port}');
+});
